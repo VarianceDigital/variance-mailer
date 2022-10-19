fd88fe7e626c13bb49a0c9fb10f43475ae07e9cd
RinaldoNani <60207094+RinaldoNani@users.noreply.github.com>
2022-10-19 15:09:06 +0200

    sitemap and robots fixed

--- variancemailer/bl_home.py
+++ variancemailer/bl_home.py
@@ -29,5 +29,5 @@ def about():
 @bp.route('/robots.txt')
 @bp.route('/sitemap.xml')
 def static_from_root():
-    return send_from_directory('static', filename=request.path[1:])
+    return send_from_directory(current_app.static_folder, request.path[1:])
 
